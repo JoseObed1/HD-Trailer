4237d6da75cea33d47937712c49b79b5db685942
Ronald Cabrera González <ronald.cabrera.gonzalez@gmail.com>
2021-08-25 18:57:17 -0400

    v1.2
    *Added more functionality and visuals

---
+++ HD-Trailer/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HD_Trailer
+{
+    public class CodigoDB
+    {
+        DBEntity db = new DBEntity();
+
+        #region Cuentas
+        public void VisualizarDatos(string id)
+        {
+            usuarios cuentas = db.usuarios.Find(id);
+            System.Diagnostics.Debug.WriteLine(cuentas.username.ToString());
+        }
+
+        public void NuevoUsuario(usuarios datos)
+        {
+            db.usuarios.Add(datos);
+            db.SaveChanges();
+        }
+
+        public void EditarUsuario(usuarios elemento)
+        {
+            db.Entry(elemento).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void BorrarUsuario(string id)
+        {
+            usuarios tempo = db.usuarios.Find(id);
+            db.usuarios.Remove(tempo);
+            db.SaveChanges();
+        }
+
+        /*
+        public bool ValidarLogin(string correo, string contra)
+        {
+            var existe = db.Cuentas.FirstOrDefault(tempolog => tempolog.Correo == correo && tempolog.Password == contra);
+
+            if (existe != null)
+            {
+                System.Diagnostics.Debug.WriteLine("X-potato!");
+                return true;
+            }
+
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Sad Programmer Noises");
+                return false;
+            }
+        }*/
+        #endregion
+
+
+        #region Foros
+        public void NuevoVideo(youtube datos)
+        {
+            db.youtube.Add(datos);
+            db.SaveChanges();
+        }
+        #endregion
+    }
+}
--- HD-Trailer/Controllers/HomeController.cs
+++ HD-Trailer/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace HD_Trailer.Controllers
 {
     public class HomeController : Controller
     {
+        CodigoDB testeo = new CodigoDB();
         DBEntity db = new DBEntity();
 
         public ActionResult Index()
@@ -15,23 +17,67 @@ namespace HD_Trailer.Controllers
             return View(db.youtube.ToList());
         }
 
-        public ActionResult About()
+        public ActionResult ForosPublicados()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(db.youtube.ToList());
         }
 
         public ActionResult sobrenosotros()
         {
             return View();
+        }
+
+        public ActionResult LoginRegister()
+        {
+            ViewBag.Notification = TempData["error2"];
+            ViewBag.ErrorLogin = TempData["error"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registro(usuarios dato)
+        {
+            if (db.usuarios.Any(x => x.username == dato.username))
+            { //Si existe en la db, dale patra
+                TempData["error2"] = "Ya existe esta cuenta";
+                return RedirectToAction("LoginRegister", "Home", TempData["error2"]);
+            }
+
+            else
+            { //Si el usuario no existe en la DB, lo creamos xD                
+                testeo.NuevoUsuario(dato);
+                Session["UserName"] = dato.username.ToString();
+                FormsAuthentication.SetAuthCookie(dato.username, true);
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InicioSesion(usuarios datoss)
+        {
+            var existe = db.usuarios.Where(y => y.username.Equals(datoss.username) && y.password.Equals(datoss.password)).FirstOrDefault();
+
+            if (existe != null)
+            {
+                Session["UserName"] = existe.username.ToString();
+                FormsAuthentication.SetAuthCookie(datoss.username, true);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                //LoginError = "Email o contraseña incorrecta";
+                TempData["error"] = "Email o contraseña incorrecta";
+                return RedirectToAction("LoginRegister", "Home", TempData["error"]);
+            }
+
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
